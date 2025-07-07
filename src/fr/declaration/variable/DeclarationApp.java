331a009c4dc601023a8cefe83d7b2e768fe49adf
Dadis <raditztv@gmail.com>
2025-07-07 11:48:31 +0200

    first commit

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,18 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args){
+        byte test1 = 1;
+        short test2 = 2;
+        int test3 = 3;
+        long test4 = 4;
+        float test5 = 5.5F;
+        double test6 = 6.6F;
+        char test7 = '7';
+        boolean test8 = true;
+        String test9 = "test9";
+        System.out.println(test9);
+        String randomString = "Voici le résultat d'un calcul :\n 1+5=6";
+        System.out.println(randomString);
+    }
+}
